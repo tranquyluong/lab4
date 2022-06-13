a98a04cce4aeed4b6c0eccafcd4e8a041c3eb482
luongtqph20440 <tranquyluong1234@gmail.com>
2022-06-13 13:42:06 +0700

    update code

--- javascript/data2.js
+++ javascript/data2.js
@@ -2,13 +2,13 @@ const productList = [
   {
     id: 1,
     name: "Fresh and Healthy Mixed Mayonnaise Salad",
-    img: "img/image 26 (1).png",
+    img: "./img/image 26 (3).png",
     time: 30,
     type: "Add To Cart",
   },
   {
     id: 2,
-    name: "The Creamiest Creamy Chicken",
+    name: "Fruity Pancake with Orange & Blueberry",
     img: "img/image 26 (2).png",
     time: 30,
     type: "Add To Cart",
@@ -23,13 +23,13 @@ const productList = [
   {
     id: 4,
     name: "Fresh and Healthy Mixed Mayonnaise Salad",
-    img: "img/image 26 (1).png",
+    img: "./img/image 26 (3).png",
     time: 30,
     type: "Add To Cart",
   },
   {
     id: 5,
-    name: "The Creamiest Creamy Chicken",
+    name: "Fruity Pancake with Orange & Blueberry",
     img: "img/image 26 (2).png",
     time: 30,
     type: "Add To Cart",
@@ -41,6 +41,7 @@ const productList = [
     time: 30,
     type: "Add To Cart",
   },
+
 ];
 
 function showProducts(products) {
@@ -50,26 +51,12 @@ function showProducts(products) {
   for (let i = 0; i < products.length; i++) {
     const product = products[i];
     result += `
-        <div>
-        <div class="product-img" >
-            <a href="./detail.html?id=${product.id}">
-                <img class="rounded-xl" src="${product.img}" alt="" />
-            </a>
-        </div>
-        <div class="product-content inline-block mt-3 w-full 
-        column shadow transition ease-in-out duration-300 hover:shadow-xl h-40">
-            <h3><a class="text-[18px] font-semibold mt-10 " href="./detail.html?id=${product.id}">${product.name}</a></h3>
-               <div>
-               <span class="font-bold text-[#F54748] inline-block" >$ ${product.time}</span>
-               </div>
-               <div class=""><a class="text-white bg-black w-full px-[94px] text-[14px] py-3 rounleading-[50px]ded-md  " href="./detail.html?id=${product.id}">
-               ${product.type}
-               </a>
-               </div>
-                
-            
-        </div>
-    </div>
+       <div class="shadow transition hover:shadow-xl group">
+            <a href=""><img src="${product.img}" alt="" class="rounded-2xl"></a>
+                <p class="pt-2 font-bold text-sm"><a href="">${product.name}</a></p>
+                <p class="text-red-600 pt-2">${product.time}</p>
+                <button class="w-full bg-black text-white p-1 rounded mt-2 ">${product.type}</button>
+            </div>
         `;
   }
   return result;
